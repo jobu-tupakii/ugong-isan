a7c4c82e39a8044f563499816fdae8b602302a6c
jobu-tupakii <dandyhong1012@gmail.com>
2025-04-08 22:03:27 +0900

    Add files via upload

--- practice14.c
+++ practice14.c
@@ -8,14 +8,18 @@ struct Student {
 
 int main() {
     int i;
-    struct Student s;
-    printf("학생 정보 입력: \n");
-    printf("이름: ");
-    scanf("%s", &s.name);
-    printf("나이: ");
-    scanf("%d", &s.age);
-    printf("학번: ");
-    scanf("%d", &s.id);
+    struct Student s[3];
+    for (int i=0; i<3; i++) {
+        printf("학생 %d 정보 입력: \n", i+1);
+        printf("이름: ");
+        scanf("%s", s[i].name);
+        printf("나이: ");
+        scanf("%d", s[i].age);
+        printf("학번: ");
+        scanf("%d", s[i].id);
+    }
     printf("\n[입력한 학생 정보 출력]\n");
-    printf("이름: %s, 나이: %d, 학번: %d", s.name, s.age, s.id);
+    for (i=0; i<3; i++) {
+        printf("이름: %s, 나이: %d, 학번: %d\n", s[i].name, s[i].age, s[i].id);
+    }
 }
