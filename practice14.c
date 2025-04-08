5852d715129f1cf96357ca8c4648b47d023261b8
jobu-tupakii <dandyhong1012@gmail.com>
2025-04-08 21:56:05 +0900

    Add files via upload

---
+++ practice14.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct Student {
+    char name [20];
+    int age;
+    int id;
+};
+
+int main() {
+    int i;
+    struct Student s;
+    printf("학생 정보 입력: \n");
+    printf("이름: ");
+    scanf("%s", &s.name);
+    printf("나이: ");
+    scanf("%d", &s.age);
+    printf("학번: ");
+    scanf("%d", &s.id);
+    printf("\n[입력한 학생 정보 출력]\n");
+    printf("이름: %s, 나이: %d, 학번: %d", s.name, s.age, s.id);
+}
